939b1e5666505541104ec087fe034a142f96ea16
prasanna1126 <venkatperumalla1992@gmail.com>
2018-11-19 10:46:29 +0530

    Users List updated

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,14 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-// Import Containers
 import { DefaultLayoutComponent } from './containers';
-
 import { P404Component } from './views/error/404.component';
 import { P500Component } from './views/error/500.component';
 import { LoginComponent } from './views/login/login.component';
 import { RegisterComponent } from './views/register/register.component';
-
 export const routes: Routes = [
   {
     path: '',
